4c13b6f962a66821f1e142e1e658cff896f41f4f
arturo-source <arturo.com>
2020-03-26 19:08:07 +0100

    Comunicación entre cliente y servidor hecha

--- client/user.go
+++ client/user.go
@@ -5,8 +5,12 @@ import (
 	"crypto/cipher"
 	"crypto/rand"
 	"crypto/sha256"
+	"crypto/tls"
 	"fmt"
 	"io"
+	"net/http"
+	"net/url"
+	"os"
 )
 
 type user struct {
@@ -31,6 +35,8 @@ func (u *user) SignIn(username, password string) {
 	u.username = username
 	u.Hash(password)
 
+	u.SendToServer("login")
+
 	//TODO: Check if the user exists in the server db
 }
 
@@ -38,7 +44,9 @@ func (u *user) SignUp(username, password string) {
 	u.username = username
 	u.Hash(password)
 
-	//TODO: Create the user in the server db
+	u.SendToServer("register")
+
+	//TODO: Create the user in the server db and check if its done Ok
 }
 
 func (u *user) EncryptFile(content []byte) ([]byte, error) {
@@ -82,6 +90,28 @@ func (u *user) DecryptFile(content []byte) ([]byte, error) {
 
 	return decryptedContent, nil
 }
-func (u *user) SendToServer(content []byte) {
-	//TO DO
+
+//SendToServer returns an error if there is an error and true if the message arrived well
+func (u *user) SendToServer(comand string) error {
+	data := url.Values{}
+	data.Set("comand", comand)
+	data.Set("username", u.username)
+	data.Set("passwd", string(u.passwordLogInServerHassed))
+
+	// Not verifying the credentials because they are autosigned
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+
+	r, err := client.PostForm("https://localhost:10443", data)
+	if err != nil {
+		return err
+	}
+	io.Copy(os.Stdout, r.Body)
+	fmt.Println()
+
+	//TODO: Check if the comunication has done Ok and return that
+
+	return nil
 }
--- server/main.go
+++ server/main.go
@@ -1,7 +1,99 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+type user struct {
+	username       string
+	passwordHashed []byte
+}
+
+func (u *user) Hash(password string) {
+	hash := sha256.New()
+	_, err := hash.Write([]byte(password))
+	if err != nil {
+		panic(err)
+	}
+	u.passwordHashed = hash.Sum(nil)
+}
+
+func (u *user) CompareHash(passwordToCompare string) bool {
+	hash := sha256.New()
+	_, err := hash.Write([]byte(passwordToCompare))
+	if err != nil {
+		panic(err)
+	}
+	passwordHashed := hash.Sum(nil)
+
+	return bytes.Compare(u.passwordHashed, passwordHashed) == 0
+}
+
+// Group of users registered on the server
+var users map[string]user
+
+// Response type to comunicate with the client
+type resp struct {
+	Ok  bool
+	Msg string
+}
+
+func response(w io.Writer, ok bool, msg string) {
+	r := resp{Ok: ok, Msg: msg}
+	rJSON, err := json.Marshal(&r)
+	if err != nil {
+		panic(err)
+	}
+	w.Write(rJSON)
+}
 
 func main() {
-	fmt.Println("Hello!")
+	http.HandleFunc("/", handler)
+
+	err := http.ListenAndServeTLS(":10443", "cert.pem", "key.pem", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func handler(w http.ResponseWriter, req *http.Request) {
+	req.ParseForm()
+	w.Header().Set("Content-Type", "text/plain")
+
+	switch req.Form.Get("comand") { // The requested action
+	case "register":
+		u := user{}
+		u.username = req.Form.Get("username")
+		password := req.Form.Get("passwd")
+
+		u.Hash(password)
+
+		_, ok := users[u.username] // Is the user in the db?
+		if ok {
+			response(w, false, "Usuario ya registrado")
+		} else {
+			users[u.username] = u
+			response(w, true, "Usuario registrado")
+		}
+
+	case "login":
+		u, ok := users[req.Form.Get("username")] // Is the user in the db?
+		if ok {
+			password := req.Form.Get("passwd")
+			if u.CompareHash(password) { // The password hashed match
+				response(w, true, "Credenciales válidas")
+			} else {
+				response(w, false, "Credenciales inválidas")
+			}
+		} else {
+			response(w, false, "Usuario inexistente")
+			return
+		}
+	default:
+		response(w, false, "Comando inválido")
+	}
 }
