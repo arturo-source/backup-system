0e408fb688c5b7a4126ba2ec6157c4a6fedf2218
arturo-source <arturo.com>
2020-04-15 18:42:55 +0200

    Funcionamiento de los tokens implementado pero no testeado

---
+++ server/token.go
@@ -0,0 +1,76 @@
+package main
+
+import "time"
+
+//Token is the struct to control when a user can access it's data
+type Token struct {
+	value      string
+	finishDate time.Time
+	userName   string
+}
+
+//Tokens implements the functions to control the tokens in the server
+type Tokens struct {
+	tokens []Token
+}
+
+//Exists returns true and the position if finds the the token in the array
+//but the token cannot be expired
+func (t *Tokens) Exists(tokenValue string) (int, bool) {
+	for i, token := range t.tokens {
+		if token.value == tokenValue && !isOutdated(token) {
+			return i, true
+		}
+	}
+	return -1, false
+}
+
+//Add adds a token to the array to this user
+//the token is valid till 1 day
+func (t *Tokens) Add(username string) {
+	tomorrow := time.Now().Add(24 * time.Hour)
+	value := string(RandStringBytes())
+	token := Token{
+		value:      value,
+		finishDate: tomorrow,
+		userName:   username,
+	}
+	t.tokens = append(t.tokens, token)
+}
+
+//Delete returns true if finds the token, and deletes it.
+func (t *Tokens) Delete(token Token) bool {
+	i, exists := t.Exists(token.value)
+	if exists {
+		t.tokens[i] = t.tokens[len(t.tokens)-1]
+		t.tokens = t.tokens[:len(t.tokens)-1]
+		return true
+	}
+
+	return false
+}
+
+//DeleteExpireds looks all tokens and delete expireds
+func (t *Tokens) DeleteExpireds() {
+	for i, token := range t.tokens {
+		if isOutdated(token) {
+			t.tokens[i] = t.tokens[len(t.tokens)-1]
+			t.tokens = t.tokens[:len(t.tokens)-1]
+		}
+	}
+}
+
+//Owner returns the username of a token and true
+//or empty string and false if it doesn't exist
+func (t *Tokens) Owner(tokenValue string) (string, bool) {
+	i, exists := t.Exists(tokenValue)
+	if exists {
+		return t.tokens[i].userName, true
+	}
+	return "", false
+}
+
+//isOutdated returns true if the token has expired
+func isOutdated(token Token) bool {
+	return time.Now().After(token.finishDate)
+}
